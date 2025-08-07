0ce2bc1c2aee7503c7a0eb77ecaa64ec3c31b9b2
Iago Jorge <iagokjorge@gmail.com>
2025-08-07 15:38:46 -0300

    Criando o componente logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
+
       <Container>
-        <Heading>Logo</Heading>
+        <Heading>MENU</Heading>
       </Container>
     </>
   );
